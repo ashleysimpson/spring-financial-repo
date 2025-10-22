87bd55810b4b5de0105066e70546a86fc8b986d7
Ashley Simpson <ashley.simpson@mail.mcgill.ca>
2025-10-22 10:42:27 -0700

    add basic database connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
-export default function Home() {
+import { neon } from "@neondatabase/serverless";
+
+export default function Page() {
+  async function create(formData: FormData) {
+    "use server";
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get("comment");
+    // Insert the comment from the form into the Postgres database
+    await sql.query("INSERT INTO comments (comment) VALUES ($1)", [comment]);
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center sm:items-start">
-          <h1 className="mb-4 text-4xl font-bold text-zinc-900 dark:text-white sm:text-5xl">
-            Spring Finanical Demo App
-          </h1>
-        </div>
-      </main>
-    </div>
+    <form action={create}>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
